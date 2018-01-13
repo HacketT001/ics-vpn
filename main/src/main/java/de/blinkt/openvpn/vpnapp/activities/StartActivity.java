621ef15f37de51b6ddbe1ab6a20a12bf39844473
Vladyslav Boiko <vladyslavboiko98@gmail.com>
2018-01-13 10:42:57 +0200

    bug fixes

---
+++ main/src/main/java/de/blinkt/openvpn/vpnapp/activities/StartActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2012-2018 Arne Schwabe
+ * Distributed under the GNU GPL v2 with additional terms. For full terms see the file doc/LICENSE.txt
+ */
+
+package de.blinkt.openvpn.vpnapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
